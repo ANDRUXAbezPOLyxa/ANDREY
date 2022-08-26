a73cda6c50eeea0951c3b56b67775faf1d65d3b3
ANDRUXA <artiswall@mail.ru>
2022-08-26 23:26:09 +0300

    good task 3

---
+++ LessonTusk3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите первое число");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int n2 = int.Parse(Console.ReadLine());
+if (n1 % n2 == 0)
+{
+Console.WriteLine("число " + n2 + " кратно " + n1);
+}
+else
+{
+int n3 = n1 % n2;
+Console.WriteLine("число " + n2 + " не кратно " + n1 + " остаток " + n3);
+}
