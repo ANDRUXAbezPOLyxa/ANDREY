ad01c4b1107c81b88324b57361a389aa73790d97
ANDRUXA <artiswall@mail.ru>
2022-08-26 22:50:59 +0300

    succesful attempt

---
+++ lesson task/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(10,100);
+int a1 = number / 10;
+int a2 = number % 10;
+int max = a1;
+if (a2 > max)
+{
+    max= a2;
+}
+Console.WriteLine("Максимальная цифра в числе" + number + "=>" + max);
