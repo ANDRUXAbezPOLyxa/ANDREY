12a5c2c9a96016f08d8c2fddb1cfb45418c22d0a
ANDRUXA <artiswall@mail.ru>
2022-08-27 00:29:58 +0300

    Lesson2Tusk5

---
+++ LessonTusk5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (b*b == a)
+{
+    Console.WriteLine("первое число " + "[" + a + "]" + " является квадратом второго " + "[" + b + "]");
+}
+if (b*b != a)
+{
+    Console.WriteLine("первое число " + "[" + a + "]" + "  не является квадратом второго " + "[" + b + "]");
+}
