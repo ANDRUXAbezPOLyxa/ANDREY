84645679d4813aed4ab160a9499be57c3e011eb4
ANDRUXA <artiswall@mail.ru>
2022-08-27 00:17:22 +0300

    Lesson2Tusk4

---
+++ LessonTask4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("введите число");
+int n = int.Parse(Console.ReadLine());
+if (n % 7 == 0)
+{
+    int n1 = n % 23;
+    if (n1 == 0)
+    {
+        Console.WriteLine("число " + n + " кратно 7 и 23");
+    }    
+    if (n1 !=0)
+    {
+        Console.WriteLine("число " + n + " не кратно 7 и 23");
+    }
+}  
+if (n % 7 !=0)
+{
+int n2 = n % 23;
+    if (n2 !=0)
+    {
+    Console.WriteLine("число " + n + " не кратно 7 и 23");
+    }
+    Console.WriteLine("число " + n + " не кратно 7 и 23");
+}      
+
